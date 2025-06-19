87956c4b13d29f70a3af6337769c4b472e56ee1b
Ananya K V <Ananya.K.V@ibm.com>
2025-06-19 15:13:21 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-setInterval(() => {
-  console.log(`[INFO] App is running at ${new Date().toISOString()}`);
-  console.error(`[ERROR] Sample error at ${new Date().toISOString()}`);
-}, 5000);
+let logging = false;
+let intervalRef = null;
 
 app.get('/', (req, res) => {
   res.send('Hello from dummy app!');
+});
+
+app.get('/start-logs', (req, res) => {
+  if (!logging) {
+    intervalRef = setInterval(() => {
+      console.log(`[INFO] App is running at ${new Date().toISOString()}`);
+      console.error(`[ERROR] Sample error at ${new Date().toISOString()}`);
+    }, 5000);
+    logging = true;
+    res.send('Logging started');
+  } else {
+    res.send('⚠️ Already logging');
+  }
+});
+
+app.get('/stop-logs', (req, res) => {
+  if (logging) {
+    clearInterval(intervalRef);
+    logging = false;
+    res.send('Logging stopped');
+  } else {
+    res.send('Logging not active');
+  }
 });
 
 app.listen(port, () => {
